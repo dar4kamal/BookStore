dcaf0f3166c43460dd4bb96c52f5af0ff1737b96
dar4kamal <sasaa9646@gmail.com>
2019-04-26 23:21:23 +0200

    extended review length

--- src/app/reviews/schemas/create.schema.js
+++ src/app/reviews/schemas/create.schema.js
@@ -3,9 +3,7 @@ const Joi = require("joi");
 const createSchema = Joi.object().keys({
     productId: Joi.string().required(),
     userId: Joi.string().required(),
-    review: Joi.string().required(),
+    review: Joi.string().min(4).max(500).required(),
 })
 
-module.exports = {
-    createSchema
-}
+module.exports = createSchema;

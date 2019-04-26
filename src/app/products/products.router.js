b03d4074d785b70260f29f03341df129c90313e0
dar4kamal <sasaa9646@gmail.com>
2019-04-26 21:04:37 +0200

    allowd only admin to manupilate products

--- src/app/products/products.router.js
+++ src/app/products/products.router.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const controller = require('./products.controller');
-
+const { role } = require('../auth/middlewares');
 const resource = '/products';
 const router = express.Router();
 
 
 router.route(resource)
     .get(controller.getAll)
-    .post(controller.create);
+    .post(role("admin"), controller.create);
 
 router.route(resource + '/:id')
     .get(controller.get)
-    .put(controller.replace)
-    .patch(controller.update)
-    .delete(controller.remove);
+    .put(role("admin"), controller.replace)
+    .patch(role("admin"), controller.update)
+    .delete(role("admin"), controller.remove);
 
 module.exports =  router;

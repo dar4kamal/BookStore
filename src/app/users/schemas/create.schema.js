640f343a3fc5c6316136a8c70fdbe91317af958e
dar4kamal <sasaa9646@gmail.com>
2019-04-26 17:52:14 +0200

    modified email in user schema

--- src/app/users/schemas/create.schema.js
+++ src/app/users/schemas/create.schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const createSchema = Joi.object().keys({
     username: Joi.string().min(3).max(30).required(),
-    email: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().max(256).required(),
     password: Joi.string().min(8).max(60).required(),
     role: Joi.string().default("user", "Customer Init")
 });
--- src/app/users/schemas/update.schema.js
+++ src/app/users/schemas/update.schema.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const updateSchema = Joi.object().keys({
     username: Joi.string().min(3).max(30),
-    email: Joi.string().min(3).max(60),    
+    email: Joi.string().email().max(256),
     role: Joi.string().default("user", "Customer Init")
 });
 

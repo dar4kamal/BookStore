f2ba858d93fd17b9710634249b3913e16b1db721
dar4kamal <sasaa9646@gmail.com>
2019-04-29 23:14:53 +0200

    added cart user schema

--- src/app/users/schemas/create.schema.js
+++ src/app/users/schemas/create.schema.js
@@ -1,18 +1,20 @@
 const Joi = require('joi');
 const productSchema = require('../../products/schemas/update.schema')
 
+const cartItem = Joi.object().keys({
+    productId: Joi.string(),
+    product: productSchema,
+    quantity: Joi.number()
+})
+
 const createSchema = Joi.object().keys({
     username: Joi.string().min(3).max(30).required(),
     email: Joi.string().email().max(256).required(),
     password: Joi.string().min(8).max(60).required(),
     role: Joi.string().default("user", "Customer Init"),
-    cart: Joi.object({
-        items:[{
-            product: productSchema,
-            quantity: Joi.number().default(0)
-        }],
-        total: Joi.number().default(0)})
-    .default({items:[],total:0}, "initiate cart")
+    cart: Joi.object().keys({
+        items: Joi.array().items(cartItem),
+        total: Joi.number()})
 });
 
 module.exports = createSchema;
--- src/app/users/schemas/update.schema.js
+++ src/app/users/schemas/update.schema.js
@@ -1,17 +1,19 @@
 const Joi = require('joi');
 const productSchema = require('../../products/schemas/update.schema')
 
+const cartItem = Joi.object().keys({
+    productId: Joi.string(),
+    product: productSchema,
+    quantity: Joi.number()
+})
 
 const updateSchema = Joi.object().keys({
     username: Joi.string().min(3).max(30),
     email: Joi.string().email().max(256),
     role: Joi.string().default("user", "Customer Init"),
-    cart:Joi.object({
-        items:[{
-            product: productSchema,
-            quantity: Joi.number().default(0)
-        }],
-        total: Joi.number().default(0)})
+    cart: Joi.object().keys({
+        items: Joi.array().items(cartItem),
+        total: Joi.number()})
 });
 
 module.exports = updateSchema;

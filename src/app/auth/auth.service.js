d1062cf99bd582c156ebcfada6de7e6556a173a5
dar4kamal <sasaa9646@gmail.com>
2019-05-11 02:06:27 +0200

    added email to jwt

--- src/app/auth/auth.service.js
+++ src/app/auth/auth.service.js
@@ -1,47 +1,46 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const document = 'users';
+const document = "users";
 
-const login = async function loginFn(dbAdapter, data, secret){
-    try {
-        const username = data.username;
-        const password = data.password;
+const login = async function loginFn(dbAdapter, data, secret) {
+  try {
+    const username = data.username;
+    const password = data.password;
 
-        const user = await dbAdapter.get(document, {username});
+    const user = await dbAdapter.get(document, { username });
 
-        if(user){
+    if (user) {
+      const res = await bcrypt.compare(password, user.password);
+      if (res) {
+        // SIGNING OPTIONS
+        const signOptions = {
+          issuer: "National Book Store Inc.",
+          audience: "http://NationalBookStore.inc",
+          subject: "anonymous",
+          algorithm: "HS256",
+          expiresIn: "1h"
+        };
 
-            const res = await bcrypt.compare(password, user.password);
-            if (res) {
+        const payload = {
+          userId: user._id,
+          email: user.email,
+          username,
+          role: user.role
+        };
 
-                // SIGNING OPTIONS
-                const signOptions = {
-                    issuer:  'National Book Store Inc.',
-                    audience: "http://NationalBookStore.inc",
-                    subject: "anonymous",
-                    algorithm: "HS256",
-                    expiresIn: "1h"
-                };
+        const token = jwt.sign(payload, secret, signOptions);
 
-                const payload = {
-                    userId: user._id,
-                    username,
-                    role:user.role
-                }
-
-                const token = jwt.sign(payload, secret, signOptions);
-                
-                return {
-                    token
-                };
-            }            
-        }
-    } catch(err) {
-        throw err;
+        return {
+          token
+        };
+      }
     }
-}
+  } catch (err) {
+    throw err;
+  }
+};
 
 module.exports = {
-    login
+  login
 };
